1e08dcc985a00f85ebe5aecb83cace6a53edba29
Yazdan Jahedi <yzdn.jt@gmail.com>
2023-12-24 17:41:38 +0330

    Add Order Entity

---
+++ TiGet_Backend/Domain/Entities/Order.cs
@@ -0,0 +1,25 @@
+﻿using Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public record Order : BaseEntity
+    {
+        public required Guid CustomerId { get; set; }
+        public Customer? Customer { get; set; }
+        public required int SitPos { get; set; }
+        public required Guid TicketId { get; set; }
+        public Ticket? Ticket { get; set; }
+
+        public required string TicketOwnerFirstName { get; set; }
+        public required string TicketOwnerLastName { get; set; }
+        public required string NatinalId { get; set; }
+
+
+        // time of transaction == createdDate
+    }
+}
