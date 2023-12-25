f878b39af31c374f262d2bfa2616832c00203449
zahra mokhtari <mokhtariii.zahraaa@gmail.com>
2023-12-25 21:04:32 +0330

    Create DTOs

---
+++ TiGet_Backend/Application/DTOs/UserDTOs.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs
+{
+    public class UserRegisterDTO
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string PhoneNumber { get; set; }
+        public Role Role { get; set; }
+    }
+
+    public class UserLoginDTO
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
