2d8d2f1e9b8e9f6ba01a9b8c1cfc785954643db0
zahra mokhtari <mokhtariii.zahraaa@gmail.com>
2023-12-25 20:40:33 +0330

    delete last version

--- TiGet_Backend/Presentation/Application/DTO/AccountViewModels/LogoutViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Models.AccountViewModels
-{
-    public class LogoutViewModel
-    {
-        public string LogoutId { get; set; }
-        public bool ShowLogoutPrompt { get; set; }
-        
-    }
-}
--- TiGet_Backend/Presentation/Application/DTO/ForgotPasswordDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Identity.API.Application.DTO
-{
-    public class ForgotPasswordDTO
-    {
-        public string Username { get; set; }
-    }
-}
