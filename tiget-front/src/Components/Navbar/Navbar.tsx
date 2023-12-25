5152a65005cf4d4517af9653e2a3ee35184bd35e
sinatb <sinatb.dev@gmail.com>
2023-12-25 22:16:02 +0330

    added links to navbar

--- tiget-front/src/Components/Navbar/Navbar.tsx
+++ tiget-front/src/Components/Navbar/Navbar.tsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import Button from '../Button/Button';
 import "./Navbar.css";
+import { Link, useNavigate } from 'react-router-dom';
+
 const Navbar = () => {
+  const navigate = useNavigate();
   return (
     <div className='navbar'>
-        <Button text='ورود / ثبت نام' onClick={()=>true}/>
-        <div className='navbar__text'><span>پشتیبانی</span></div>
-        <div className='navbar__text'><span>سبد خرید</span></div>
-        <div className='navbar__text'><span>جستجوی بلیت ها</span></div>
-        <div className='navbar__logo'>Tiget</div>
+        <Button text='ورود / ثبت نام' onClick={()=>navigate("/login")}/>
+        <div className='navbar__text'><Link className='link' to="/support"><span>پشتیبانی</span></Link></div>
+        <div className='navbar__text'><Link className='link' to="/buypage"><span>سبد خرید</span></Link></div>
+        <div className='navbar__text'><Link className='link' to="/search"><span>جستجوی بلیت ها</span></Link></div>
+        <div className='navbar__logo'><Link className='link' to="/">Tiget</Link></div>
     </div>
   )
 }
