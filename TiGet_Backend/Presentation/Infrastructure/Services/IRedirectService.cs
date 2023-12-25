ffcb526ef75dca1de21ff419f4a30155311ef3ca
zahra mokhtari <mokhtariii.zahraaa@gmail.com>
2023-12-25 20:18:40 +0330

    delete unused files

--- TiGet_Backend/Presentation/Infrastructure/Services/IRedirectService.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Rhazes.Services.Identity.API.Infrastructure.Services
-{
-    public interface IRedirectService
-    {
-        string ExtractRedirectUriFromReturnUrl(string url);
-    }
-}
