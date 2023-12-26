39194a530bdca4654be6e7de900289a823407a9c
sinatb <sinatb.dev@gmail.com>
2023-12-26 18:03:23 +0330

    company login added to login page

--- tiget-front/src/Pages/LoginPage/LoginPage.tsx
+++ tiget-front/src/Pages/LoginPage/LoginPage.tsx
@@ -2,13 +2,16 @@ import React, { useState, useContext } from 'react';
 import { UserContext } from '../../Components/UserProvider/UserProvider';
 import Button from '../../Components/Button/Button';
 import './LoginPage.css';
-import { sina } from '../../FakeData/fakeData';
+import { sag_Company, sina } from '../../FakeData/fakeData';
 import { useNavigate } from 'react-router-dom';
-const Login = () =>{
+const Login : React.FC<{loginType:boolean}> = ({loginType}) =>{
     const { login,userData } = useContext(UserContext);
     const navigate = useNavigate();
     const handleLogin = () => {
-            login(sina);
+            if (loginType)
+                login(sina);
+            else
+                login(sag_Company);
             navigate('/account');
       };
     return <form className='login-form'>
@@ -17,21 +20,31 @@ const Login = () =>{
         <Button text='ورود' onClick={handleLogin}/>
     </form>
 }
-const SignUp = () => {
+const SignUp : React.FC<{loginType:boolean}> = ({loginType}) => {
+    const handleSignUp = () => {
+        return true;
+    }
     return <form className='login-form'>
         <input type='text' placeholder='نام کاربری'/>
         <input type='text' placeholder='رمز عبور'/>
         <input type='text' placeholder='تکرار رمز عبور'/>
         <input type='text' placeholder='ایمیل'/>
-        <Button text='ساخت اکانت' onClick={()=>true}/>
+        <Button text='ساخت اکانت' onClick={handleSignUp}/>
     </form>
 }
 const LoginPage = () => 
 {
     const [loginState,setLoginState] = useState<boolean>(true);
+    // 0 means user login
+    // 1 means company login
+    const [loginType,setLoginType] = useState<boolean>(true);
 
-    function toggleState(){
+
+    const toggleState = () => {
         setLoginState(!loginState);
+    }
+    const toggleType = () => {
+        setLoginType(!loginType);
     }
     return (
         <div>
@@ -40,13 +53,20 @@ const LoginPage = () =>
             </div>
             <div className='form-holder'>
                 {loginState? 
-                (<Login/>): 
-                (<SignUp/>)
+                (<Login loginType={loginType} />): 
+                (<SignUp loginType={loginType} />)
                 }
                 <div className='login-signup-toggle'>
                     {loginState? 
                     <span onClick={toggleState}>ساخت حساب کاربری جدید</span>:
                     <span onClick={toggleState}>قبلا ثبت نام کرده‌اید</span>}
+                </div>
+                <div className='user-company-toggle'>
+                    {
+                        loginType?
+                        <span onClick={toggleType}>ورود کاربر</span> :
+                        <span onClick={toggleType}>ورود شرکت</span>
+                    }
                 </div>
             </div>
         </div>
