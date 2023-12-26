25e2ac2dcb94e3e66fedffd975f512f60d9240a3
sanoooavi <81512968+sanoooavi@users.noreply.github.com>
2023-12-26 10:30:00 +0330

    changed font-family and color

--- tiget-front/src/App.tsx
+++ tiget-front/src/App.tsx
@@ -1,12 +1,13 @@
-import React from 'react';
-import LandingPage from './Pages/LandingPage/LandingPage';
-import LoginPage from './Pages/LoginPage/LoginPage';
-import SearchPage from './Pages/SearchPage/SearchPage';
-import AccountPage from './Pages/AccountPage/AccountPage';
+import React from "react";
+import LandingPage from "./Pages/LandingPage/LandingPage";
+import LoginPage from "./Pages/LoginPage/LoginPage";
+import SearchPage from "./Pages/SearchPage/SearchPage";
+import AccountPage from "./Pages/AccountPage/AccountPage";
+import BuyPage from "./Pages/BuyPage/BuyPage";
 function App() {
   return (
-    <div >
-      <AccountPage/>
+    <div>
+      <BuyPage />
     </div>
   );
 }
