ae7576054503a9c3b227561c6b38f5a28908ef0c
sinatb <sinatb.dev@gmail.com>
2023-12-26 15:39:02 +0330

    navbar context added

--- tiget-front/src/Components/Navbar/Navbar.tsx
+++ tiget-front/src/Components/Navbar/Navbar.tsx
@@ -2,12 +2,15 @@ import React from 'react'
 import Button from '../Button/Button';
 import "./Navbar.css";
 import { Link, useNavigate } from 'react-router-dom';
-
+import { useContext } from 'react';
+import { UserContext } from '../UserProvider/UserProvider';
 const Navbar = () => {
+  const {isLoggedIn} = useContext(UserContext);
+
   const navigate = useNavigate();
   return (
     <div className='navbar'>
-        <Button text='ورود / ثبت نام' onClick={()=>navigate("/login")}/>
+        {isLoggedIn ? <div className='account-photo' onClick={()=>navigate('/account')}> </div> : <Button text='ورود / ثبت نام' onClick={()=>navigate("/login")}/>}
         <div className='navbar__text'><Link className='link' to="/support"><span>پشتیبانی</span></Link></div>
         <div className='navbar__text'><Link className='link' to="/buypage"><span>سبد خرید</span></Link></div>
         <div className='navbar__text'><Link className='link' to="/search"><span>جستجوی بلیت ها</span></Link></div>
