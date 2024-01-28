f83178ec5867c659008a49deec235524e89a3df4
sinatb <sinatb.dev@gmail.com>
2024-01-28 23:32:27 +0330

    added update fields for customer, changed company page

--- tiget-front/src/Pages/AccountPage/UserDataCard.tsx
+++ tiget-front/src/Pages/AccountPage/UserDataCard.tsx
@@ -1,13 +1,21 @@
-import React, { useState } from 'react';
-import { Customer, Company } from '../../FakeData/fakeData';
-import Button from '../../Components/Button/Button';
+import React, { useState } from "react";
+import { Customer, Company, Role } from "../../FakeData/fakeData";
+import Button from "../../Components/Button/Button";
+import axios from "axios";
 
-export const UserDataCard: React.FC<{ account: Customer | Company | null; }> = ({ account }) => {
-  
-  const [name, setName] = useState(account === null ? "" : (account as Customer).firstName);
-  const [lastName, setLastName] = useState(account === null ? "" : (account as Customer).lastName);
+export const UserDataCard: React.FC<{ account: Customer | Company | null }> = ({
+  account,
+}) => {
+  const [name, setName] = useState(
+    account === null ? "" : (account as Customer).firstName
+  );
+  const [lastName, setLastName] = useState(
+    account === null ? "" : (account as Customer).lastName
+  );
   const [email, setEmail] = useState(account === null ? "" : account.email);
-  const [phoneNumber, setPhoneNumber] = useState(account === null ? "" : account.phoneNumber);
+  const [phoneNumber, setPhoneNumber] = useState(
+    account === null ? "" : account.phoneNumber
+  );
   const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setName(e.target.value);
   };
@@ -20,32 +28,73 @@ export const UserDataCard: React.FC<{ account: Customer | Company | null; }> = (
   const handlePhoneNumberChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setPhoneNumber(e.target.value);
   };
-  
-  return <> 
-  {
-    account === null ? <div></div> :
-    <div className='data-card'>
-      <form className='data-card-form'>
-        <div className='data-card-holder'>
-          <div>نام </div>
-          <input className='data-card-input' value={name} onChange={handleNameChange} />
+
+  const handleChange = () => {
+    axios
+      .put("http://localhost:5120/api/customer/update", {
+        headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
+        email: email,
+        phoneNumber: phoneNumber,
+        firstName: name,
+        lastName: lastName,
+      })
+      .then(() => console.log("change done"))
+      .catch();
+  };
+  return (
+    <>
+      {account === null ? (
+        <div></div>
+      ) : (
+        <div className="data-card">
+          <form className="data-card-form">
+            <div className="data-card-holder">
+              {account.role === Role.User ? (
+                <div>نام </div>
+              ) : (
+                <div>نام شرکت</div>
+              )}
+              <input
+                className="data-card-input"
+                value={name}
+                onChange={handleNameChange}
+              />
+            </div>
+            {account.role === Role.Company ? (
+              <div></div>
+            ) : (
+              <div className="data-card-holder">
+                <div>نام خانوادگی </div>
+                <input
+                  className="data-card-input"
+                  value={lastName}
+                  onChange={handleLastNameChange}
+                />
+              </div>
+            )}
+            <div className="data-card-holder">
+              <div>ایمیل</div>
+              <input
+                className="data-card-input"
+                value={email}
+                onChange={handleEmailChange}
+              />
+            </div>
+            <div className="data-card-holder">
+              <div>شماره همراه</div>
+              <input
+                className="data-card-input"
+                value={phoneNumber}
+                onChange={handlePhoneNumberChange}
+                style={{ direction: "ltr" }}
+              />
+            </div>
+          </form>
+          <div className="account-acceptance-button">
+            <Button text="اعمال تغییرات" onClick={handleChange} />
+          </div>
         </div>
-        <div className='data-card-holder'>
-          <div>نام خانوادگی </div>
-          <input className='data-card-input' value={lastName} onChange={handleLastNameChange} />
-        </div>
-        <div className='data-card-holder'>
-          <div>ایمیل</div>
-          <input className='data-card-input' value={email} onChange={handleEmailChange} />
-        </div>
-        <div className='data-card-holder'>
-          <div>شماره همراه</div>
-          <input className='data-card-input' value={phoneNumber} onChange={handlePhoneNumberChange} style={{ direction: 'ltr' }} />
-        </div>
-      </form>
-      <div className='account-acceptance-button'>
-        <Button text='اعمال تغییرات' onClick={() => true} />
-      </div>
-    </div>}
-  </>;
+      )}
+    </>
+  );
 };
